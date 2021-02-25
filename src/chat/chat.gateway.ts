159f10ec7031486448f84dce0075e599eb5cf598
Jakub Stasiak <jstasiak97@gmail.com>
2021-02-25 20:10:55 +0100

    chore: add logs to chat

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,29 +1,35 @@
-import { UseGuards } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Logger, UseGuards } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
 import { ChatService } from './chat.service';
 
 // @UseGuards(JwtAuthGuard)
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway(3001)
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('ChatGateway');
 
   constructor(private readonly chatService: ChatService) { }
 
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: Socket, payload: any): void {
+    this.logger.log(`Got message, emiting message:client with payload ${payload}`);
     this.server.emit('message:client', payload);
   }
 
-  @SubscribeMessage('connect')
-  handleConnection(client: Socket, payload: any) {
-    this.server.emit('user:connected', payload);
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+    this.server.emit('user:connected', client.id);
   }
 
-  @SubscribeMessage('disconnect')
-  handleDisconnect(client: Socket, payload: any) {
-    this.server.emit('user:disconnected', payload);
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+    this.server.emit('user:disconnected', client.id);
   }
 }
