cc549c93be6488ef6d8d31647ae44c9c2228f079
Jakub Stasiak <jstasiak97@gmail.com>
2021-11-27 17:57:19 +0100

    feat: add return statement to on socket init

--- src/socket.ts
+++ src/socket.ts
@@ -2,10 +2,12 @@ import type { Server } from 'http';
 import { Server as SocketIoServer, Socket } from 'socket.io';
 import { chatHandler } from './modules/web-sockets/chat';
 
-export const initSocketServer = (server: Server) => {
+export const initSocketServer = (server: Server): SocketIoServer => {
   const io = new SocketIoServer(server);
   const onConnection = (socket: Socket) => {
     chatHandler(io, socket);
   };
   io.on('connection', onConnection);
+
+  return io;
 };
