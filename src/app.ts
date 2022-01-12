1d41648e914536592b97a41f4e679012bccc2e5e
Jakub Stasiak <jstasiak97@gmail.com>
2022-01-12 19:01:31 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import { router as userRouter } from './modules/user';
 import { router as authRouter } from './modules/auth';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true
