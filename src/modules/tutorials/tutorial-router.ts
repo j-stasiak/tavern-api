1af9e731650828d69cc719266af28ea325337a6c
Jakub Stasiak <jstasiak97@gmail.com>
2022-08-08 19:38:39 +0200

    feat: add complete tutorial route

--- src/modules/tutorials/tutorial-router.ts
+++ src/modules/tutorials/tutorial-router.ts
@@ -1,12 +1,14 @@
-import express from 'express';
-import { validate, validateParams } from '../../middleware/request-validator';
+import { complete, getAllTutorials, getTutorial, postTutorial } from './tutorial-controller';
 import { createTutorialSchema, getTutorialSchema } from './schemas';
-import { getTutorial, postTutorial, getAllTutorials } from './tutorial-controller';
+import { validate, validateParams } from '../../middleware/request-validator';
+
+import express from 'express';
 
 const router = express.Router();
 
 router.post('/', validate(createTutorialSchema), postTutorial);
 router.get('/', getAllTutorials);
 router.get('/:id', validateParams(getTutorialSchema), getTutorial);
+router.post('/:id/complete', complete);
 
 export { router };
