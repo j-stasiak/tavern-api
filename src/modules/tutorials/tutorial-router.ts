091301313b660036eb0640ac304f3556802fb416
Jakub Stasiak <jstasiak97@gmail.com>
2022-08-21 22:28:39 +0200

    feat: add tutorial route

--- src/modules/tutorials/tutorial-router.ts
+++ src/modules/tutorials/tutorial-router.ts
@@ -1,4 +1,4 @@
-import { complete, getAllTutorials, getTutorial, postTutorial } from './tutorial-controller';
+import { complete, getAllTutorials, getTutorial, patchTutorial, postTutorial } from './tutorial-controller';
 import { createTutorialSchema, getTutorialSchema } from './schemas';
 import { validate, validateParams } from '../../middleware/request-validator';
 
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post('/', validate(createTutorialSchema), postTutorial);
 router.get('/', getAllTutorials);
+router.patch('/:id', patchTutorial);
 router.get('/:id', validateParams(getTutorialSchema), getTutorial);
 router.post('/:id/complete', complete);
 
